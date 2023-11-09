41fdab23cb30759f427bea95bd275c8717c0b429
Admir Šaheta <admirxsaheta@gmail.com>
2023-11-09 12:36:40 +0100

    merge/from/crust

---
+++ examples/class.cpp
@@ -0,0 +1,35 @@
+class A {
+  int a;
+  int b;
+
+private:
+  float getfloat() { return 1.23; }
+
+public:
+  int getInt() { return a; }
+
+  A() {
+    a = 5;
+    b = 6;
+  }
+};
+
+class B {
+  int aa, bb;
+};
+
+class Address {
+  int id;
+  char name;
+  char postal;
+  int pin;
+};
+
+int main() {
+  B b = B();
+  A a = A();
+  a.getInt();
+  Address add = Address();
+
+  // do some thing else
+}
---
+++ examples/struct.cpp
@@ -0,0 +1,18 @@
+struct FilePointer {
+  int stdin;
+  int stdout;
+  int stderr;
+  char EOF;
+};
+
+struct A {
+  int a, b, c;
+  float fa;
+};
+
+int main() {
+  struct FilePointer fp;
+  struct A decl_a;
+
+  // do something with fp here
+}
